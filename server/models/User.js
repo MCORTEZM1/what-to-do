aa3c16f3ffa73a723e62e7b788155dfe2fc8206d
Michael Cortez Macbook <bayarea6fifty@gmail.com>
2022-12-10 13:32:26 -0800

    included update hash for password

--- server/models/User.js
+++ server/models/User.js
@@ -53,6 +53,17 @@ userSchema.pre('save', async function(next) {
 }); 
 
 
+userSchema.pre('findOneAndUpdate', async function(next){
+    if ( this._update.password) { 
+        const saltRounds = 10; 
+        this._update.password = await bcrypt.hash(this._update.password, saltRounds);
+        next();
+    }
+
+    next(); 
+}) 
+
+
 const User = model('User', userSchema);
 
 module.exports = User
